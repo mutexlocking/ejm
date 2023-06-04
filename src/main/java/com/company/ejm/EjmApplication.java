28310b8f2575d59fac4818b3c43d93fdd08f786d
mutexlocking <kkt1692@konkuk.ac.kr>
2023-06-04 14:09:41 +0900

    [FIRST COMMIT]

---
+++ src/main/java/com/company/ejm/EjmApplication.java
@@ -0,0 +1,13 @@
+package com.company.ejm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjmApplication.class, args);
+	}
+
+}
