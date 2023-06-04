b646e2b1e3932fb9f89047bcab9b1b2ef9830a2a
mutexlocking <kkt1692@konkuk.ac.kr>
2023-06-04 16:10:43 +0900

    [추가] : CommonCodeGroupRepository 정의

---
+++ src/main/java/com/company/ejm/group/repository/CommonCodeGroupRepository.java
@@ -0,0 +1,7 @@
+package com.company.ejm.group.repository;
+
+import com.company.ejm.group.CommonCodeGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommonCodeGroupRepository extends JpaRepository<CommonCodeGroup, Long> {
+}
