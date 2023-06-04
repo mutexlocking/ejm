4798e8c3408b4e3e3d9933319df6d8ebc5cf36ab
mutexlocking <kkt1692@konkuk.ac.kr>
2023-06-04 15:58:59 +0900

    [추가] : CommonCode 정의

--- src/main/java/com/company/ejm/common/response/ApiResponseStatus.java
+++ src/main/java/com/company/ejm/common/response/ApiResponseStatus.java
@@ -3,6 +3,31 @@ package com.company.ejm.common.response;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+/**
+ * [ 1000단위 ] - 오류의 범위
+ *  1000 : 요청 성공
+ *  2 : Request 오류
+ *  3 : Reponse 오류
+ *  4 : DB 오류
+ *  5 : Server 오류
+ *
+ * [ 100단위 ] - 오류 도메인
+ *  0 : 공통 오류
+
+ *
+ *
+ * [10단위] - 오류 HTTP Method
+ *  0~19 : Common
+ *  20~39 : GET
+ *  40~59 : POST
+ *  60~79 : PATCH
+ *  80~99 : else
+ *
+ *  [1 단위] - 그외 오류의 고유 식별자
+ *          - 순서대로 설정해주면 됨
+ *
+ */
+
 @Getter
 public enum ApiResponseStatus {
 
@@ -18,7 +43,7 @@ public enum ApiResponseStatus {
     INCORRECT_HTTP_METHOD(false, HttpStatus.BAD_REQUEST, 2003, "잘못 매칭된 HTTP 메소드로 요청이 들어왔습니다."),
 
 
-    INTERNAL_SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR, 4000, "예상하지 못한 예외가 발생하였습니다.");
+    INTERNAL_SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR, 5000, "예상하지 못한 예외가 발생하였습니다.");
 
 
 
